ef11c853b4cd0126b5557fb6d52cb6289c60fdb6
HJ Park <b2aconnn@gmail.com>
2025-07-18 15:31:24 +0900

    feat: 포인트 E2E 테스트 및 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/support/validation/DateConverter.java
@@ -0,0 +1,27 @@
+package com.loopers.support.validation;
+
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class DateConverter {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static LocalDate convertToLocalDate(String dateString) {
+        if (Objects.isNull(dateString) || dateString.isBlank()) {
+            throw new IllegalArgumentException(dateString + "을(를) 입력해주세요.");
+        }
+
+        try {
+            return LocalDate.parse(dateString, DATE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(dateString + " 형식이 올바르지 않습니다. (yyyy-MM-dd 형식)");
+        }
+    }
+}
