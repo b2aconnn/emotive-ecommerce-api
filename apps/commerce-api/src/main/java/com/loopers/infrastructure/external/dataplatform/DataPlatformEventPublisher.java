b8474a861cf6e58e103c0ab2b2a772b1360ef26f
HJ Park <b2aconnn@gmail.com>
2025-08-29 16:32:13 +0900

    feat: 주문, 결제의 결과에 대한 데이터 플랫폼에 전송하는 후속처리를 진행

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/external/dataplatform/DataPlatformEventPublisher.java
@@ -0,0 +1,27 @@
+package com.loopers.infrastructure.external.dataplatform;
+
+import com.loopers.application.order.event.model.CreateOrderEvent;
+import com.loopers.application.payment.event.model.PaymentResultEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@RequiredArgsConstructor
+@Component
+public class DataPlatformEventPublisher {
+
+    private final DataPlatformClient dataPlatformClient;
+
+    @Async("dataPlatformExecutor")
+    @TransactionalEventListener
+    public void publishOrderData(CreateOrderEvent event) {
+        dataPlatformClient.sendOrderData(event);
+    }
+
+    @Async("dataPlatformExecutor")
+    @TransactionalEventListener
+    public void publishPaymentData(PaymentResultEvent payment) {
+        dataPlatformClient.sendPaymentData(payment);
+    }
+}
