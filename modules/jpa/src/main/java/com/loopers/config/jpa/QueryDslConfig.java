e460316e69997e0c0531b864027fc1d218aaade5
HJ Park <b2aconnn@gmail.com>
2025-09-02 19:49:02 +0900

    feat: ecommerce-streamer, Kafka 모듈 추가

--- modules/jpa/src/main/java/com/loopers/config/jpa/QueryDslConfig.java
+++ modules/jpa/src/main/java/com/loopers/config/jpa/QueryDslConfig.java
@@ -2,9 +2,11 @@ package com.loopers.config.jpa;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 class QueryDslConfig {
